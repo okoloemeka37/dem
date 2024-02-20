57f456b3f9073c7835e38e31b681c0ed01631eef
okoloemeka37 <okoloemeka37@gmail.com>
2024-02-20 22:38:58 +0100

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+composer global require hirak/prestissimo
+
 
 echo "generating application key..."
 php artisan key:generate --show
